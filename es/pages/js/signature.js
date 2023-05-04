dcbdb8bdc8b5fead0e650273d13d23add1b3ae91
Edward L. Campbell <77728779+ecampbelldsp@users.noreply.github.com>
2023-05-04 15:50:06 +0200

    init

--- es/pages/js/signature.js
+++ es/pages/js/signature.js
@@ -108,17 +108,17 @@
     if (e.target == canvas) {
       e.preventDefault();
     }
-  }, {passive: false});
+  }, false);
   document.body.addEventListener("touchend", function(e) {
     if (e.target == canvas) {
       e.preventDefault();
     }
-  }, {passive: false});
+  }, false);
   document.body.addEventListener("touchmove", function(e) {
     if (e.target == canvas) {
       e.preventDefault();
     }
-  }, {passive: false});
+  }, false);
 
   (function drawLoop() {
     requestAnimFrame(drawLoop);

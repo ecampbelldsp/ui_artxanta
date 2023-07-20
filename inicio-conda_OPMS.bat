287d4ffb93c0947f8c29d4db774df131b0546d43
RobertoNovo <nuestrong@gmail.com>
2023-07-20 14:46:07 +0200

    Cambios LOPD y gramática

--- inicio-conda_OPMS.bat
+++ inicio-conda_OPMS.bat
@@ -1,6 +1,6 @@
 call C:\Users\IVIGO48\anaconda3\Scripts\activate.bat C:\Users\IVIGO48\anaconda3
 call conda activate opencheck
-call cd C:\Opencheck\redforts_artxanda
+call cd C:\Opencheck\OPMS_artxanda
 call set FLASK_APP = app.py
 call set FLASK_DEBUG = 1
 call flask run
